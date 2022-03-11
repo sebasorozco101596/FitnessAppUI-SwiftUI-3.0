2ca48335c7fb2e6f1b98562c6a8bd575c7a8a00f
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-03-11 18:22:03 -0500

    Initial Commit

---
+++ FitnessAppUI-SwiftUI-3.0/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FitnessAppUI-SwiftUI-3.0
+//
+//  Created by Juan Sebastian Orozco Buitrago on 3/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FitnessAppUI-SwiftUI-3.0/FitnessAppUI_SwiftUI_3_0App.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppUI_SwiftUI_3_0App.swift
+//  FitnessAppUI-SwiftUI-3.0
+//
+//  Created by Juan Sebastian Orozco Buitrago on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppUI_SwiftUI_3_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
